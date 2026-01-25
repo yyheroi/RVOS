7ed480f233a642e062e7fcc71678b98bfdfdc849
yyheroi <crystalyyhero@gmail.com>
2026-01-25 21:00:16 +0800

    <feat>(ins): Add main RISC-V instruction visualization window

---
+++ lib/ins/inc/Gui/RISCVInstructionWindow.hh
@@ -0,0 +1,32 @@
+#ifndef RISCV_INSTRUCTION_WINDOW_HH
+#define RISCV_INSTRUCTION_WINDOW_HH
+
+#include <gtkmm.h>
+#include <string>
+#include <giomm.h>  // Gio::Application
+#include <glibmm.h> // Glib::RefPtr
+#include "Core/Instruction.hh"
+#include "Gui/InstFormatUI.hh"
+
+class RISCVInstructionWindow : public Gtk::Window {
+public:
+    Gtk::Box *uiContainer_      = nullptr;
+    Gtk::Entry *InsEntry_       = nullptr;
+    Gtk::Button *InsButtonParse_= nullptr;
+    Gtk::TextView *InsTextView_ = nullptr;
+    Instruction *pInst_         = nullptr;
+    InstFormatUI *rTypeUI_      = nullptr;
+
+public:
+    RISCVInstructionWindow();
+    ~RISCVInstructionWindow()= default;
+
+private:
+    void initInstFormatUI();
+    void onInsButtonParseClicked();
+    void showInsResult(Instruction &inst);
+    void showError(const std::string &message);
+    void loadCssFromFile(Gtk::Window &window);
+};
+
+#endif // RISCV_INSTRUCTION_WINDOW_HH
